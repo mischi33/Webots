c290fa349421486fabb4f0fb17d8a6f902430c06
Michelle <michelle.weber@students.fhv.at>
2017-12-04 10:41:22 +0100

    created packages for exercise 1 and 2

--- src/exercise2/DriveTowardsLight.java
+++ src/exercise2/DriveTowardsLight.java
@@ -1,16 +1,15 @@
+package exercise2;
+
 import com.cyberbotics.webots.controller.DifferentialWheels;
 import com.cyberbotics.webots.controller.LightSensor;
 
-
-public class DriveTowardsLightStop extends DifferentialWheels {
+public class DriveTowardsLight extends DifferentialWheels{
     private static int TIME_STEP = 15;
     private static int MAX_SPEED = 800;
     private static int MIN_SPEED = 0;
 
     private static int MAX_SENSOR_VALUE = 1300;
     private static int MAX_FRONT_SENSOR_VALUE = 400;
-    private static int STOP_VALUE = 350;
-    private static int STOP_VALUE_2 = 1500;
 
     private static int FRONT_RIGHT = 0;
     private static int FRONT_MIDDLE_RIGHT = 1;
@@ -20,31 +19,30 @@ public class DriveTowardsLightStop extends DifferentialWheels {
     private static int LEFT = 5;
     private static int FRONT_MIDDLE_LEFT = 6;
     private static int FRONT_LEFT = 7;
+
+
     private LightSensor[] lightSensors;
 
-    public DriveTowardsLightStop(){
+    public DriveTowardsLight(){
         super();
         lightSensors = new LightSensor[] { getLightSensor("ls0"), getLightSensor("ls1"),
                 getLightSensor("ls2"), getLightSensor("ls3"),
                 getLightSensor("ls4"), getLightSensor("ls5"),
                 getLightSensor("ls6"), getLightSensor("ls7") };
 
-        for (LightSensor lightSensor : lightSensors) {
-            lightSensor.enable(10);
+        for (int i = 0; i < lightSensors.length; i++) {
+            lightSensors[i].enable(10);
         }
     }
 
     public static void main(String[] args) {
-        DriveTowardsLightStop controller = new DriveTowardsLightStop();
+        DriveTowardsLight controller = new DriveTowardsLight();
         controller.run();
     }
 
     public void run() {
         while (step(TIME_STEP) != -1) {
-            if ((lightSensors[FRONT_LEFT].getValue() < STOP_VALUE || lightSensors[FRONT_RIGHT].getValue() < STOP_VALUE)
-                    && (lightSensors[FRONT_MIDDLE_LEFT].getValue() > STOP_VALUE_2 && lightSensors[FRONT_MIDDLE_RIGHT].getValue() > STOP_VALUE_2)) {
-                stopDriving();
-            } else if (lightSensors[FRONT_LEFT].getValue() < MAX_FRONT_SENSOR_VALUE &&
+            if (lightSensors[FRONT_LEFT].getValue() < MAX_FRONT_SENSOR_VALUE &&
                     lightSensors[FRONT_RIGHT].getValue() < MAX_FRONT_SENSOR_VALUE) {
                 driveStraightAhead();
             } else if (lightSensors[FRONT_MIDDLE_LEFT].getValue() < MAX_SENSOR_VALUE
@@ -59,10 +57,6 @@ public class DriveTowardsLightStop extends DifferentialWheels {
                 driveStraightAhead();
             }
         }
-    }
-
-    private void stopDriving() {
-        setSpeed(MIN_SPEED, MIN_SPEED);
     }
 
     private void driveToRight() {
