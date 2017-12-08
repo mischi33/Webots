8056909d242da8cd15feff00dae15bc02e385770
Flowkii <florian.braun@students.fhv.at>
2017-12-08 14:04:19 +0100

    added drive along a wall Proportional

--- src/BangBangDriveAlongTheWall.java
+++ src/BangBangDriveAlongTheWall.java
@@ -12,21 +12,15 @@ public class BangBangDriveAlongTheWall extends BangBangController {
     private static int FRONT_RIGHT = 0;
     private static int FRONT_MIDDLE_RIGHT = 1;
     private static int RIGHT = 2;
-    private static int FRONT_LEFT = 7;
+    private static int FRONT_LEFT = 4;
 
 
     private DistanceSensor[] distanceSensors;
 
     public BangBangDriveAlongTheWall() {
         super();
-        distanceSensors = new DistanceSensor[]{getDistanceSensor("ps0"), getDistanceSensor("ps1"),
-                getDistanceSensor("ps2"), getDistanceSensor("ps3"),
-                getDistanceSensor("ps4"), getDistanceSensor("ps5"),
-                getDistanceSensor("ps6"), getDistanceSensor("ps7")};
+        initDistanceSensors(new String[]{"ps0","ps1","ps2","ps7"});
 
-        for (DistanceSensor distanceSensor : distanceSensors) {
-            distanceSensor.enable(10);
-        }
 
     }
 
--- src/ProportionalDriveAlongTheWall.java
+++ src/ProportionalDriveAlongTheWall.java
@@ -1,11 +1,54 @@
 
 public class ProportionalDriveAlongTheWall extends RobotController {
 
+    private static double controllerValue = 0.2;
+    private static int MAX_SPEED = 100;
+
+
     public ProportionalDriveAlongTheWall() {
+        super();
+        String[] sensorNames = new String[]{"ps6", "ps0", "ps2", "ps7","ps5","ps1"};
+        initDistanceSensors(sensorNames);
     }
 
-    @Override
-    public void run() {
+    private void setSpeedValues(double left, double right) {
+        setSpeed(left, right);
+    }
 
+    public void run() {
+        while (step(TIME_STEP) != -1) {
+            double left = calcSpeedLeft(new String[] {"ps1", "ps2"});
+            double right = calcSpeedRight(new String[] {"ps6", "ps2","ps1"});
+            setSpeedValues(left, right);
+        }
+
+    }
+    private double calcSpeedRight(String[] sensors) {
+        double speed = 0;
+
+        //Rechter Sensor +LinksVorne Sensorn +RechtsVorne Sensor -2xLinkerSensor(um die Schwankungen der Sensoren zu minimieren) + 7* (SchrägRechtsVorne Sensor-Rechter Sensor/2) (7* damit er schnell genug dreht und -Rechter Sensor damit er sich nicht im Kreis dreht)
+        speed =controllerValue * (getDistanceSensor("ps2").getValue()+getDistanceSensor("ps7").getValue()+getDistanceSensor("ps6").getValue()-2*getDistanceSensor("ps5").getValue()+7*(getDistanceSensor("ps1").getValue()-getDistanceSensor("ps2").getValue()/2))+400;
+
+        if (speed > 1000) {
+            speed = 1000;
+        }
+        return speed;
+    }
+
+
+    private double calcSpeedLeft(String[] sensors) {
+        double speed = 0;
+
+        speed =controllerValue * (getDistanceSensor("ps2").getValue()-getDistanceSensor("ps7").getValue()-getDistanceSensor("ps6").getValue()+3*getDistanceSensor("ps5").getValue()-getDistanceSensor("ps0").getValue())+400;
+
+        if (speed > 1000) {
+            speed = 1000;
+        }
+        return speed;
+    }
+
+    public static void main(String[] args) {
+        ProportionalDriveAlongTheWall controller = new ProportionalDriveAlongTheWall();
+        controller.run();
     }
 }
